8aee94c73a4727e0082dd448131f489c7f6b719a
Melissa Cadena <melissabcadena@Melissas-MBP.attlocal.net>
2020-06-03 21:16:33 -0500

    fix mixed case and blank responses

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,33 +1,39 @@
+var fightOrSkip = function() {
+    // ask user if they'd like to fight or skip using  function
+    var promptFight = window.prompt('Would you like FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
+    promptFight = promptFight.toLowerCase();
+
+    if (promptFight === "" || promptFight === null) {
+        window.alert("You need to provide a valid answer! Please try again.");
+        return fightOrSkip();
+      }
+  
+    // if user picks "skip" confirm and then stop the loop
+    if (promptFight === "skip") {
+      // confirm user wants to skip
+      var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+  
+      // if yes (true), leave fight
+      if (confirmSkip) {
+        window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
+        // subtract money from playerMoney for skipping
+        playerInfo.playerMoney = playerInfo.money - 10;
+        return true;
+      }
+      else {
+          return false;
+      }
+    }
+  }
+
 var fight = function (enemy) {
 
     // repeat and execute as long as the enemy robot is alive 
     while (enemy.health > 0 && playerInfo.health > 0) {
-
-        var promptFight = window.prompt("Would you like to FIGHT or SKIP this batte? Enter 'FIGHT' or 'SKIP' to choose.");
-
-        if (promptFight === "skip" || promptFight === "SKIP") {  
-            
-            // confirm the user would like to skip
-            var confirmSkip = window.confirm("Are you sure you'd like to quit?");
-
-            //if yes (true), leave fight
-            if (confirmSkip) {
-                window.alert(playerInfo.name + " has chosen to skip the fight. Goodbye!");
-
-                //subtract mmoney from playerInfo.money for skipping
-                playerInfo.money = Math.max(0, playerInfo.money - 10);
-                console.log("playerInfo.money", playerInfo.money);
+            if (fightOrSkip()) {
+                //if true, leave fight by breaking loop
                 break;
             }
-            // if no (False), ask question again by running fight() again
-            else {
-                fight();
-            }
-        }
-        // if player chooses to fight, then fight 
-        if (promptFight === "fight" || promptFight === "FIGHT") {
-
-            // generate random damage value based on player's attach power
             var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
             enemy.health = Math.max(0, enemy.health - damage);
 
@@ -62,16 +68,12 @@ var fight = function (enemy) {
             else {
                 window.alert(playerInfo.name + " still has " + playerInfo.health + " health left.");
             }
-        
-        } else {
-            window.alert("You need to pick a valid option. Try again!");
-        };
     }
 }
 
 //function to start a new game
 var startGame = function() {
-    
+
     // reset player stats 
     playerInfo.reset();
 
@@ -86,6 +88,9 @@ var startGame = function() {
             
             // reset enemy.ealth before starting new fight
             pickedEnemyObj.health = randomNumber(40, 60);
+
+            //use debugger to pause script from running and check what's goingon at that moment in the code 
+            //debugger;
 
             // pass the pickedenemy.name variable's value into the fight function, where it will assume the value of the enemy.name parameter
             fight(pickedEnemyObj);
@@ -173,21 +178,8 @@ var randomNumber = function(min, max) {
     return value;
 };
 
-// function to set name
-
-var getPlayerName = function() {
-    var name = "";
-
-    while (name === "" || name === null) {
-        name = prompt("What is your robot's name?");
-    }
-
-    console.log("Your robot's name is " + name);
-    return name;
-};
-
 var playerInfo = {
-    name: getPlayerName(),
+    name: window.prompt("What is your robot's name?"),
     health: 100,
     attack: 10,
     money: 10,
@@ -234,6 +226,7 @@ var enemyInfo = [
 ];
 
 // start the game when the page loads
+//debugger;
 startGame();
 
         // after the loop ends, player is either out of health or enemies
